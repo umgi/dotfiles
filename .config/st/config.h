4a387d182a030bec84aa28a0bc98cb0ac503e7d2
umgi <gtdsocial@icloud.com>
2021-05-02 20:25:04 +0300

    fix: vim scroll freezing

--- .config/st/config.h
+++ .config/st/config.h
@@ -1,3 +1,4 @@
+/* vim: set ts=8 sw=8 tw=0 noet : */
 /* See LICENSE file for copyright and license details. */
 
 /*
@@ -20,7 +21,7 @@ static int borderpx = 2;
 static char *shell = "/bin/sh";
 char *utmp = NULL;
 /* scroll program: to enable use a string like "scroll" */
-char *scroll = "scroll";
+ char *scroll = NULL;
 char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 
 /* identification sequence returned in DA and DECID */
@@ -225,6 +226,8 @@ ResourcePref resources[] = {
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
+	{ XK_ANY_MOD,		Button4, kscrollup,      {.i = 1},	0,	-1},
+	{ ShiftMask,		Button5, kscrolldown,    {.i = 1},	0,	-1},
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
@@ -269,6 +272,10 @@ static Shortcut shortcuts[] = {
 	{ MODKEY,               XK_y,           externalpipe,   {.v = copyurlcmd } },
 	{ MODKEY,               XK_o,           externalpipe,   {.v = copyoutput } },
 	{ TERMMOD,              XK_Y,           zoom,           {.f = +5} },
+
+	{ ShiftMask,		XK_Page_Up,	kscrollup,	{.i = -1} },
+	{ ShiftMask,		XK_Page_Down,	kscrolldown,	{.i = -1} },
+
 };
 
 /*
--- .config/st/st.h
+++ .config/st/st.h
@@ -84,6 +84,9 @@ void die(const char *, ...);
 void redraw(void);
 void draw(void);
 
+void kscrolldown(const Arg *);
+void kscrollup(const Arg *);
+
 void externalpipe(const Arg *);
 void printscreen(const Arg *);
 void printsel(const Arg *);
@@ -91,6 +94,7 @@ void sendbreak(const Arg *);
 void toggleprinter(const Arg *);
 
 int tattrset(int);
+int tisaltscr(void);
 void tnew(int, int);
 void tresize(int, int);
 void tsetdirtattr(int);
