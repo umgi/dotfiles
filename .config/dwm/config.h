a0ea50463ca995380fd2783530e4d15706a2ecc2
umgi <gtdsocial@icloud.com>
2021-05-10 01:59:31 +0300

    add: dwm focusonclick

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -16,6 +16,7 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
+static int focusonwheel       = 0;
 static char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#111111";
 static char normbordercolor[]       = "#111111";
