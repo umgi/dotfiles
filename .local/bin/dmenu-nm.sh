77afe9528530c033ab43f2a4a3484a49a2e32767
umgi <gtdsocial@icloud.com>
2021-05-12 14:40:03 +0300

    fix: sh compatible dmenu-nm.sh

--- .local/bin/dmenu-nm.sh
+++ .local/bin/dmenu-nm.sh
@@ -1,18 +1,14 @@
-
-
+#!/bin/sh
 
 connections=$(nmcli connection show | sed -E '1d; s/--/down/; s/^(.*?\S)  .+  .+  ([^ ]+).+/\2 \1/' | sort)
-section=$(printf "$connections\\nnet off\nnet on" | dmenu -i -p "nm:")
-
+section=$(printf "%s\\nnet off\nnet on" "$connections" | dmenu -i -p "nm:")
 
 case $section in
   "net off") nmcli networking off ;;
   "net on") nmcli networking on ;;
   *)
-    status=$(echo $section | awk '{print $1}')
-    conn=$(echo $section | sed -E 's/^\S*? //')
-    echo $status
-    echo $conn
+    status=${section%% *}
+    conn=${section#* }
     case $status in
       "down")
         nmcli networking on
