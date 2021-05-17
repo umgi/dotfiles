41c6f6dbfbe8f0e8f4c333b3d08fb2b583530273
umgi <gtdsocial@icloud.com>
2021-05-17 07:33:47 +0300

    add: popup mpv and feh

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -61,12 +61,13 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	*/
-	/* class    instance      title       	 tags mask    isfloating   isterminal  noswallow  monitor */
-	{ "Gimp",     NULL,       NULL,       	    1 << 8,       0,           0,         0,        -1 },
-	{ TERMCLASS,   NULL,       NULL,       	    0,            0,           1,         0,        -1 },
-	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
-	{ NULL,      "spterm",    NULL,       	    SPTAG(0),     1,           1,         0,        -1 },
-	{ NULL,      "spcalc",    NULL,       	    SPTAG(1),     1,           1,         0,        -1 },
+	/* class	instance	title			tags mask	isfloating	isterminal	noswallow	monitor */
+	{ "Gimp",	NULL,		NULL,			1 << 8,		0,		0,		0,		-1 },
+	{ TERMCLASS,	NULL,		NULL,			0,		0,		1,		0,		-1 },
+	{ NULL,		NULL,		"Event Tester",		0,		0,		0,		1,		-1 },
+	{ NULL,		"spterm",	NULL,			SPTAG(0),	1,		1,		0,		-1 },
+	{ NULL,		"spcalc",	NULL,			SPTAG(1),	1,		1,		0,		-1 },
+	{ NULL,		NULL,		"popup",		0,		1,		0,		1,		-1 },
 };
 
 /* layout(s) */
