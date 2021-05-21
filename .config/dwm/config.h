0fc3641bae45cd46071bbbb89abdfee5b2e5b1a4
umgi <gtdsocial@icloud.com>
2021-05-21 22:03:51 +0300

    add: gaps

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -6,16 +6,16 @@
 #define TERMCLASS "St"
 
 /* appearance */
-static unsigned int borderpx  = 2;        /* border pixel of windows */
-static unsigned int snap      = 16;       /* snap pixel */
-static unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static int showbar            = 1;        /* 0 means no bar */
-static int topbar             = 1;        /* 0 means bottom bar */
+static unsigned int borderpx  = 2;	/* border pixel of windows */
+static unsigned int snap      = 16;	/* snap pixel */
+static unsigned int gappih    = 80;	/* horiz inner gap between windows */
+static unsigned int gappiv    = 80;	/* vert inner gap between windows */
+static unsigned int gappoh    = 80;	/* horiz outer gap between windows and screen edge */
+static unsigned int gappov    = 80;	/* vert outer gap between windows and screen edge */
+static int swallowfloating    = 0;	/* 1 means swallow floating windows by default */
+static int smartgaps          = 0;	/* 1 means no outer gap when there is only one window */
+static int showbar            = 1;	/* 0 means no bar */
+static int topbar             = 1;	/* 0 means bottom bar */
 static int focusonwheel       = 0;
 static char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
 
