ff9b786e6749ce8ee7f205731af87edfce63217f
umgi <gtdsocial@icloud.com>
2021-06-22 12:06:13 +0300

    add: f2_4 ctrl_x_v

--- .config/nvim/auto.vim
+++ .config/nvim/auto.vim
@@ -1,6 +1,6 @@
 autocmd BufWritePost ~/.config/dwmblocks/config.h !cd ~/.config/dwmblocks/; sudo make install && {killall -q dwmblocks;setsid dwmblocks & }
 autocmd BufWritePost ~/.config/dwm/config.h !cd ~/.config/dwm/; sudo make install && {killall -q dwm;setsid dwm & }
-autocmd BufWritePost ~/.config/sxhkd/sxhkdrc !{killall -q sxhkd; setsid -f sxhkd }
+autocmd BufWritePost ~/.config/sxhkd/sxhkdrc !{pkill -USR1 sxhkd}
 
 
 " xrdb
--- .local/bin/remaps.sh
+++ .local/bin/remaps.sh
@@ -12,23 +12,9 @@ killall xcape
 xcape -t 200 -e "Super_L=Tab;Control_L=Escape"
 
 
-# Not now
 
 # KP_Enter = Enter
 # xmodmap -e "keycode 104 = Return"
-# Left_Shift = Shift_Lock
+# Shift_Lock
 # xmodmap -e 'keycode 50 = Shift_Lock'
-# Right_Shift = Shift_Lock
 # xmodmap -e 'keycode 62 = Shift_Lock'
-
-# Not working
-# Tab = Tab
-# Tab + <key> = Super_L + <key>
-# xmodmap -e "keycode 133 = Super_L"
-# xmodmap -e "keycode 23 = Tab"
-# xcape -e "Super_L=Tab" -t $timeout
-
-# Caps_Lock = Escape
-# Caps_Lock + <key> = Control_L + <key>
-# xmodmap -e "keycode 66 = Control_L"
-# xcape -t "200" -e "Control_L=Escape"
