a97e3949845ab9e94e8cb95942c83c26e51f8a00
umgi <gtdsocial@icloud.com>
2021-09-25 18:37:34 +0300

    add: free storage space

--- .config/dwmblocks/config.h
+++ .config/dwmblocks/config.h
@@ -27,6 +27,7 @@ static const Block blocks[] = {
 	{"",	"sb-clock",	60,	1},
 	{"",	"sb-help-icon",	0,	15},*/
 
+	{ "",	"df -h --output=avail /dev/sda2 | tail -n1",	3600,	15 },
 	{ "",	PATH("sb-pomodoro"),	15,	15 },
 	{ "",	PATH("sb-load"),	15,	11 },
 	{ "",	PATH("sb-volume"),	0,	10 },
