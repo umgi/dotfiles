f9e3bef45050ebea59cfac14cb44b60eb5e45294
umgi <gtdsocial@icloud.com>
2022-02-15 23:31:20 +0300

    add: mpv font

--- .local/bin/theme.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-hsetroot -solid "$(xrdb -query | grep 'desktop.background' | cut -f 2)"
-
-
-echo "source $XDG_CONFIG_HOME/nvim/schemes/$(xrdb -query | grep 'nvim.scheme' | cut -f 2)" > "$XDG_CONFIG_HOME/nvim/schemes/index.vim"
