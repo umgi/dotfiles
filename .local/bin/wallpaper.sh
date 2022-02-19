e9b5059c1c83952e25ffccf6900503a882d0ffef
umgi <gtdsocial@icloud.com>
2022-02-19 18:26:30 +0300

    add: dether random bg

--- .local/bin/wallpaper.sh
+++ .local/bin/wallpaper.sh
@@ -4,4 +4,16 @@
 
 MAIN="$(xrdb -query | grep 'desktop.background1' | cut -f 2)"
 SECOND="$(xrdb -query | grep 'desktop.background2' | cut -f 2)"
-hsetroot -add "$MAIN" -add "$SECOND" -gradient 0
+WP_FOLDER="$XDG_DATA_HOME/wallpaper/"
+FILE="$(
+  find "$WP_FOLDER" -type f -print0 |
+    xargs -0 file --mime-type |
+    grep -F 'image/' |
+    cut -d ':' -f 1 |
+    shuf -n 1 )"
+echo "$FILE"
+BASENAME="$(basename "$FILE")"
+EXTNAME="${BASENAME##*.}"
+#hsetroot -add "$MAIN" -add "$SECOND" -gradient 0 -write /tmp/bg.png
+convert "$FILE" -ordered-dither o8x8,6 "/tmp/bg.$EXTNAME"
+hsetroot -cover "/tmp/bg.$EXTNAME"
