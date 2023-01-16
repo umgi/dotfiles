b521e609bbcbf2a7d1ea961eaabd44bcea4a57bb
pgeowng <gtdsocial@icloud.com>
2023-01-17 02:40:39 +0300

    openbox

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,58 +1,63 @@
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
-vim.diagnostic.config({signs = false})
+vim.diagnostic.config({ signs = false })
 
-map('n', '<leader>e', vim.diagnostic.open_float, opts)
-map('n', '[d', vim.diagnostic.goto_prev, opts)
-map('n', ']d', vim.diagnostic.goto_next, opts)
-map('n', '<leader>q', vim.diagnostic.setloclist, opts)
+map("n", "<leader>e", vim.diagnostic.open_float, opts)
+map("n", "[d", vim.diagnostic.goto_prev, opts)
+map("n", "]d", vim.diagnostic.goto_next, opts)
+map("n", "<leader>q", vim.diagnostic.setloclist, opts)
 
 local on_attach = function(client, bufnr)
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-  map('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  map('n', 'gd', vim.lsp.buf.definition, bufopts)
-  map('n', 'K', vim.lsp.buf.hover, bufopts)
-  map('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  map('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  map('n', '<leader>wl', function() 
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  map('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
-  map('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-  map('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-  map('n', 'gr', vim.lsp.buf.references, bufopts)
-  map('n', '<leader>f', vim.lsp.buf.formatting, bufopts)
+	map("n", "gD", vim.lsp.buf.declaration, bufopts)
+	map("n", "gd", vim.lsp.buf.definition, bufopts)
+	map("n", "K", vim.lsp.buf.hover, bufopts)
+	map("n", "gi", vim.lsp.buf.implementation, bufopts)
+	map("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+	map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+	map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+	map("n", "<leader>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	map("n", "<leader>D", vim.lsp.buf.type_definition, bufopts)
+	map("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+	map("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+	map("n", "gr", vim.lsp.buf.references, bufopts)
+	map("n", "<leader>f", vim.lsp.buf.formatting, bufopts)
 end
 
-local lspconfig = require'lspconfig'
-local util = require'lspconfig/util'
+local lspconfig = require("lspconfig")
+local util = require("lspconfig/util")
 
 local function config(_config)
-  return vim.tbl_deep_extend('force', {
-    capabilities = require'cmp_nvim_lsp'.update_capabilities(
-      vim.lsp.protocol.make_client_capabilities())
-    },
-    _config or {}
-  )
+	return vim.tbl_deep_extend("force", {
+		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+	}, _config or {})
 end
 
-lspconfig.gopls.setup(config{
-  on_attach = on_attach,
-  cmd = { 'gopls', 'serve' },
-  filetypes = { 'go', 'go.mod' },
-  root_dir = util.root_pattern('go.work', 'go.mod', '.git'),
-  settings = {
-    gopls = {
-      analyses = {
-        unusedparams = true,
-        shadow = true,
-      },
-      staticcheck = true,
-    }
-  }
+lspconfig.gopls.setup(config({
+	on_attach = on_attach,
+	cmd = { "gopls", "serve" },
+	filetypes = { "go", "go.mod" },
+	root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+	settings = {
+		gopls = {
+			analyses = {
+				unusedparams = true,
+				shadow = true,
+			},
+			staticcheck = true,
+		},
+	},
+}))
+
+lspconfig.ccls.setup({
+	init_options = {
+		cache = {
+			directory = "/tmp/ccls",
+		},
+	},
 })
--- .local/bin/popup.sh
+++ .local/bin/popup.sh
@@ -10,7 +10,7 @@ mpv_fit="150x150"
 #''--geometry='$size$feh_shift'
 
 case "$1" in
-  video) setsid -f sh -c "mpv --title=popup --autofit-larger='$mpv_fit' --geometry='$mpv_shift' -quiet '$2' >/dev/null 2>&1 ; [ $(echo '$?') != '0' ] && notify-send 'popup.sh' 'mpv cant open\n$2'" ;;
+  video) setsid -f sh -c "mpv --title=popup --autofit-larger='$mpv_fit' --geometry='$mpv_shift' -quiet '$2' --ytdl-raw-options=ignore-config=,sub-lang='ja',write-sub=,write-auto-sub= >~/.cache/popup-mpv.log 2>&1 ; [ $(echo '$?') != '0' ] && notify-send 'popup.sh' 'mpv cant open\n$2'" ;;
   image)
     setsid -f sh -c "feh --title 'popup' --geometry='90x90'  --scale-down '$2' >/dev/null 2>&1 "
     ;;
